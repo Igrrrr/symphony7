c9ee0716f0a9534b519b006e5334d6193ed7434e
Igor Konovodov <konovodov@list.ru>
2024-11-25 21:43:35 +0300

    removing: commented out fragments

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,3 @@
-// const slider = new Splide("#slider", {
-//   perPage: 4,
-//   gap: "1rem",
-//   flickMaxPages: 1,
-//   perPage: 3,
-//   arrows: false,
-//   breakpoints: {
-//     1199: {
-//       perPage: 2,
-//     },
-//     767: {
-//       perPage: 1,
-//     },
-//   },
-// });
-
-// const slider = new Splide("#slider", {
-//   perPage: 2,
-//   gap: "30px",
-// });
-
-// slider.mount();
-
-//import Splide from "@splidejs/splide";
-//new Splide(".splide").mount();
 const slider = new Splide("#slider", {
   perPage: 3,
   gap: "2rem",
@@ -34,8 +9,6 @@ const slider = new Splide("#slider", {
   },
 });
 slider.mount();
-
-//new Splide("#slider").mount();
 
 const anchors = document.querySelectorAll("a[href*='#']");
 anchors.forEach((anchor) => {
